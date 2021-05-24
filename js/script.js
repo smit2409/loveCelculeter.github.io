90092e7240e354b32db1917612d0cc01381ad605
Smit Bhalodiya <bhalodiyasmit2409@gmail.com>
2021-05-24 21:21:07 +0530

    warning box update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 function resultBox() {
     let first = document.getElementById('first-val').value;
     let last = document.getElementById('last-val').value;
+    let warningBox = document.getElementById('warning');
+    let warningText = document.getElementById('w-text');
+    let warningbtn = document.getElementById('w-btn');
     if (first == "") {
-        alert('Please enter your Name');
+        warningBox.style.display= "block";
+        warningText.innerText = "Please enter your Name";
     } else if (first.length <= 2) {
-        alert('Please enter your valid Name');
+        warningBox.style.display= "block";
+        warningText.innerText = "Please enter your valid Name";
     } else if (!isNaN(first)) {
-        alert('In this cannot write the namber');
+        warningBox.style.display= "block";
+        warningText.innerText = "In this cannot write the namber";
     } else if (last == "") {
-        alert('Please enter your Love Name');
+        warningBox.style.display= "block";
+        warningText.innerText = "Please enter your Love Name";
     } else if (last.length <= 2) {
-        alert('Please enter your valid Love Name');
+        warningBox.style.display= "block";
+        warningText.innerText = "Please enter your valid Love Name";
     } else if (!isNaN(last)) {
-        alert('In this cannot write the namber');
+        warningBox.style.display= "block";
+        warningText.innerText = "In this cannot write the namber";
     } else {
         let loveData = Math.floor(Math.random() * 100);
         document.getElementById('result').value = loveData + '%';
     }
+    warningbtn.addEventListener('click',function () {
+        warningBox.style.display= "none";
+    });
 }
